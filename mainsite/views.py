111cde7391ce78ce4b3d9a9c90e343336d6a8cb2
Haokun Cai <haokun@uw.edu>
2019-07-03 14:17:53 +0800

    new template

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
 from django.template.loader import get_template
-from datetime import datetime
 from django.http import HttpResponse
+from datetime import datetime
 from .models import Post
+from django.shortcuts import redirect
 
 def homepage(request):
     template = get_template('index.html')
@@ -10,3 +10,14 @@ def homepage(request):
     now = datetime.now()
     html = template.render(locals())
     return HttpResponse(html)
+
+
+def showpost(request, slug):
+    template = get_template('post.html')
+    try:
+        post = Post.objects.get(slug=slug)
+        if post != None:
+            html = template.render(locals())
+            return HttpResponse(html) 
+    except:
+        return redirect('/')
